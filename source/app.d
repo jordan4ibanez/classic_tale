49a2fda7b5f209d79d5b1515725f132ac8461eb3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-17 02:15:14 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -1,11 +1,21 @@
 import raylib;
 import std.stdio;
 
+import math.quat;
+import math.vec3d;
+
 void main() {
 	// call this before using raylib
 	validateRaylibBinding();
 	InitWindow(800, 600, "Hello, Raylib-D!");
 	SetTargetFPS(60);
+
+	Quat blah = Quat();
+
+	writeln(blah);
+
+	Vec3d boof = Vec3d();
+	writeln(boof);
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(Colors.RAYWHITE);
