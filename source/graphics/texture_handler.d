16d30e43fd67191efd19eb6f418dfcf6fb3d0cb3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-17 02:31:20 -0500

    That's supposed to be internal lmao

--- source/graphics/texture_handler.d
+++ source/graphics/texture_handler.d
@@ -39,41 +39,7 @@ public: //* BEGIN PUBLIC API.
         return (textureName in texturePointDatabase) !is null;
     }
 
-    // void drawTexture(string textureName, Vec2d position, Rect sourceOnTexture, Vec2d size, Vec2d origin = Vec2d(0, 0),
-    //     double rotation = 0) {
-
-    //     Vec2d flippedPosition = Vec2d(position.x, -position.y);
-
-    //     struct OutputRect {
-    //         int x = 0;
-    //         int y = 0;
-    //         int w = 0;
-    //         int h = 0;
-    //     }
-
-    //     OutputRect rawInput;
-    //     database.getRectangleIntegral(textureName, rawInput);
-
-    //     Rect source = Rect();
-    //     source.x = rawInput.x + cast(int) sourceOnTexture.x;
-    //     source.y = rawInput.y + cast(int) sourceOnTexture.y;
-    //     source.width = sourceOnTexture.width;
-    //     source.height = sourceOnTexture.height;
-
-    //     Rect dest = Rect(
-    //         flippedPosition.x,
-    //         flippedPosition.y,
-    //         size.x,
-    //         size.y
-    //     );
-
-    //     DrawTexturePro(atlas, source.toRaylib(), dest.toRaylib(), origin.toRaylib(), rotation, Colors
-    //             .WHITE);
-    // }
-
-    // bool hasTexture(string name) {
-    //     return database.contains(name);
-    // }
+private: //* BEGIN INTERNAL API.
 
     void loadTexture(string location, ref TexturePacker!string database) {
 
@@ -101,6 +67,4 @@ public: //* BEGIN PUBLIC API.
     void terminate() {
         UnloadTexture(*atlas);
     }
-
-private: //* BEGIN INTERNAL API.
 }
