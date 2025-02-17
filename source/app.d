dcd03f280e3ba97bcb561f8ae87d1c294a5ba89a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-17 03:02:11 -0500

    READY 4 CUBE

--- source/app.d
+++ source/app.d
@@ -1,34 +1,59 @@
 import graphics.camera_handler;
+import graphics.model_handler;
+import graphics.texture_handler;
 import raylib;
 import std.stdio;
 
 void main() {
 	// call this before using raylib
 	validateRaylibBinding();
-	InitWindow(800, 600, "Hello, Raylib-D!");
+	InitWindow(800, 600, "Classic Fable Prototyping");
+	scope (exit) {
+		CloseWindow();
+	}
 	SetTargetFPS(60);
 
 	CameraHandler.initialize();
-	scope(exit) CameraHandler.terminate();
-	
+	scope (exit) {
+		CameraHandler.terminate();
+	}
+
+	TextureHandler.initialize();
+	scope (exit) {
+		TextureHandler.terminate();
+	}
+
 	rlDisableBackfaceCulling();
 
+	float[] vertices = [
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+	];
 
+	TextureHandler.getPoints("default_stone.png");
+
+	float[] textureCoordinates = [
+
+	];
+
+	ModelHandler.newModelFromMesh("triangle", vertices, textureCoordinates);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(Colors.RAYWHITE);
 
+		DrawText("Hello, World!", 10, 10, 28, Colors.BLACK);
+
 		CameraHandler.begin();
 
 		{
-			DrawText("Hello, World!", 400, 300, 28, Colors.BLACK);
 
-			DrawCube(Vector3(0, 0, 0), 1, 1, 1, Colors.RED);
+			// DrawCube(Vector3(0, 0, 0), 1, 1, 1, Colors.RED);
 		}
 
 		CameraHandler.end();
 		EndDrawing();
 	}
-	CloseWindow();
+
 }
