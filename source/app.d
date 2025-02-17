00fc4126146ea4670994d51c5e45873a926cf7b0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-17 17:46:53 -0500

    Math things

--- source/app.d
+++ source/app.d
@@ -32,20 +32,13 @@ void main() {
 
 	rlDisableBackfaceCulling();
 
-	// static immutable Vec3d topLeftBack = Vec3d(0, 1, 0);
-	// static immutable Vec3d topRightBack = Vec3d(1, 1, 0);
-	// static immutable Vec3d topLeftFront = Vec3d(0, 1, 1);
-	// static immutable Vec3d topRightFront = Vec3d(1, 1, 1);
-
-	float[] vertices = [
-
-	];
+	float[] vertices;
 
 	void makeQuad(
-		const ref Vec3d topLeft, /*0*/
-		const ref Vec3d bottomLeft, /*1*/
-		const ref Vec3d bottomRight, /*2*/
-		const ref Vec3d topRight /*3*/ ) {
+		const Vec3d topLeft, /*0*/
+		const Vec3d bottomLeft, /*1*/
+		const Vec3d bottomRight, /*2*/
+		const Vec3d topRight /*3*/ ) {
 		// Tri 1.
 		vertices ~= topLeft.toFloatArray(); // 0
 		vertices ~= bottomLeft.toFloatArray(); // 1
@@ -54,16 +47,36 @@ void main() {
 		vertices ~= bottomRight.toFloatArray(); // 2
 		vertices ~= topRight.toFloatArray(); // 3
 		vertices ~= topLeft.toFloatArray(); // 0
-
 	}
+
+	/*
+	This is kind of weird.
+
+	Right handed means that you're looking forwards pointing at -Z.
+	Left is -X, right is +x.
+
+	But the math is using actual player coordinates so that means that Z is technically inverted.
+	But we math that right out and pretend it's normal.
+
+	So the chunk will generate behind you and to your right.
+	*/
+
+	makeQuad(
+		Vec3d(1, 1, 0),
+		Vec3d(1, 0, 0),
+		Vec3d(0, 0, 0),
+		Vec3d(0, 1, 0)
+	);
+
+	// static immutable Vec3d topLeftBack = Vec3d(0, 1, 0);
+	// static immutable Vec3d topRightBack = Vec3d(1, 1, 0);
+	// static immutable Vec3d topLeftFront = Vec3d(0, 1, 1);
+	// static immutable Vec3d topRightFront = Vec3d(1, 1, 1);
 
 	// 0.0, 1.0, 0.0, // 0
 	// 	0.0, 0.0, 0.0, // 1
 	// 	1.0, 0.0, 0.0, // 2
-
 	// 	1.0, 0.0, 0.0, // 2
-	// 	1.0, 1.0, 0.0, // 3
-	// 	0.0, 1.0, 0.0, // 0
 
 	TexPoints blah = TextureHandler.getPoints("testing.png");
 
