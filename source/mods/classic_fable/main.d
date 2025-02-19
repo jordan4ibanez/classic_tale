f0c7f1201ef41588cc8d5e2fba034168ef682aa8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-19 04:51:48 -0500

    Reflect in the main mod

--- source/mods/classic_fable/main.d
+++ source/mods/classic_fable/main.d
@@ -24,17 +24,17 @@ void cubeThingMain() {
 
     CubeThingBlock stone = new CubeThingBlock();
     stone.name = "stone";
-    stone.texture = "default_stone.png";
+    stone.textures = "default_stone.png";
     BlockDatabase.registerBlock(stone);
 
     CubeThingBlock dirt = new CubeThingBlock();
     dirt.name = "dirt";
-    dirt.texture = "default_dirt.png";
+    dirt.textures = "default_dirt.png";
     BlockDatabase.registerBlock(dirt);
 
     CubeThingBlock grass = new CubeThingBlock();
     grass.name = "grass";
-    grass.texture = "default_grass.png";
+    grass.textures = "default_grass.png";
     BlockDatabase.registerBlock(grass);
 
     //? Biomes.
