6e9cad45afe6dbae64c4a137fb8e89adf96736c0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-19 04:26:42 -0500

    First chunk

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ import graphics.camera_handler;
 import graphics.model_handler;
 import graphics.texture_handler;
 import math.vec2d;
+import math.vec2i;
 import math.vec3d;
 import raylib;
 import std.stdio;
@@ -42,6 +43,8 @@ void main() {
 	// MapGraphics.makeCube(vertices, textureCoordinates, Vec3d(0, 0, 0), Vec3d(0, 0, 0), Vec3d(1, 1, 1), faces, tex);
 
 	// ModelHandler.newModelFromMesh("triangle", vertices, textureCoordinates);
+
+	MapGraphics.generate(Vec2i(0, 0));
 
 	while (Window.shouldStayOpen()) {
 
