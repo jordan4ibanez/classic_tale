ec1a0d602b80e90dea02eb49740654ec9dcaf9f7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-20 06:58:24 -0500

    This probably does not work

--- source/math/aabb.d
+++ source/math/aabb.d
@@ -6,3 +6,17 @@ struct AABB {
     Vec3d min;
     Vec3d max;
 }
+
+bool aabbCollision(AABB a, AABB b) {
+    // Inverted exclusion detection because I'm weird.
+    if (a.max.x < b.min.x || a.min.x > b.max.x) {
+        return false;
+    }
+    if (a.max.y < b.min.y || a.min.y > b.max.y) {
+        return false;
+    }
+    if (a.max.z < b.min.z || a.min.z > b.max.z) {
+        return false;
+    }
+    return true;
+}
