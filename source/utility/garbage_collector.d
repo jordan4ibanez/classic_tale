14a8be992f7ce16ebb45513e66a9f1243cef291d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-20 03:46:57 -0500

    Create garbage_collector.d

---
+++ source/utility/garbage_collector.d
@@ -0,0 +1,37 @@
+module utility.garbage_collector;
+
+import core.memory;
+
+static final const class GarbageCollector {
+static:
+private:
+
+    immutable ulong averager = 200;
+    double[averager] gcCollection = 0;
+    ulong index = 0;
+
+public:
+
+    // I keep this here for testing.
+    // GC.disable();
+
+    // This averages out the GC heap info in megabytes so you can actually read it.
+    double getHeapInfo() {
+        gcCollection[index] = cast(double) GC.stats().usedSize / 1_000_000.0;
+
+        double total = 0;
+        foreach (size; gcCollection) {
+            total += size;
+        }
+        total /= averager;
+
+        index++;
+        if (index >= averager) {
+            index = 0;
+        }
+
+        return total;
+
+    }
+
+}
