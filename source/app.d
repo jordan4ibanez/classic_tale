8ca3de6e14689f4664f0c12c29c809e68f9f57ac
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-19 20:38:48 -0500

    Draw, le chunks

--- source/app.d
+++ source/app.d
@@ -51,6 +51,9 @@ void main() {
 	// ModelHandler.newModelFromMesh("triangle", vertices, textureCoordinates);
 
 	Map.debugGenerate(0, 0);
+	Map.debugGenerate(1, 0);
+	Map.debugGenerate(2, 0);
+	Map.debugGenerate(3, 0);
 	// Vec2i blah = Vec2i(0, 0);
 	// MapGraphics.generate(blah);
 
@@ -83,7 +86,20 @@ void main() {
 		CameraHandler.begin();
 		{
 
-			ModelHandler.draw("Chunk:0|0", Vec3d(0, 0, 0));
+			if (ModelHandler.modelExists("Chunk:0|0")) {
+				ModelHandler.draw("Chunk:0|0", Vec3d(0, 0, 0));
+			}
+			if (ModelHandler.modelExists("Chunk:1|0")) {
+
+				ModelHandler.draw("Chunk:1|0", Vec3d(CHUNK_WIDTH * 1, 0, 0));
+			}
+			if (ModelHandler.modelExists("Chunk:2|0")) {
+
+				ModelHandler.draw("Chunk:2|0", Vec3d(CHUNK_WIDTH * 2, 0, 0));
+			}
+			if (ModelHandler.modelExists("Chunk:3|0")) {
+				ModelHandler.draw("Chunk:3|0", Vec3d(CHUNK_WIDTH * 3, 0, 0));
+			}
 
 			// ModelHandler.draw("triangle", Vec3d(0, 0, 0));
 
