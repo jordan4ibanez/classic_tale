3c8bdb061b1111fd744f7b641335da4bdefe0082
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-21 03:04:23 -0500

    AABB entity constructor

--- source/math/aabb.d
+++ source/math/aabb.d
@@ -1,10 +1,41 @@
 module math.aabb;
 
+import math.vec2d;
 import math.vec3d;
+
+//? Note: Entities position is at the bottom center of the collision box.
+
+/*
+
+          |------------|< AABB Max
+          |            |
+          |            |
+          |            |
+          |            |
+          |            |
+          |            |
+          |            |
+          |            |
+AABB Min> |------X-----| 
+                 ^
+                 |-------- Actual position
+*/
 
 struct AABB {
     Vec3d min;
     Vec3d max;
+
+    /// Entity constructor.
+    this(const ref Vec3d position, const ref Vec2d size) {
+        this.min.x = position.x - size.x;
+        this.min.y = position.y;
+        this.min.z = position.z - size.x;
+
+        this.max.x = position.x + size.x;
+        this.max.y = position.y + size.y;
+        this.max.z = position.z + size.x;
+
+    }
 }
 
 bool aabbCollision(AABB a, AABB b) {
