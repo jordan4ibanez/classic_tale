9f2ed1eace78ef08b1ca15d47f9b5185da43bfa5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-21 02:56:30 -0500

    Now rewire this to intake logic for Z

--- source/utility/collision_functions.d
+++ source/utility/collision_functions.d
@@ -24,35 +24,43 @@ CollisionResult collideXZToBlock(Vec2d entityPosition, Vec2d entitySize, Vec2d e
     Vec2d blockPosition, Vec2d blockSize, CollisionAxis axis) {
 
     CollisionResult result;
-    result.newPosition = entityPosition.x;
 
-    int dir = cast(int) sgn(entityVelocity.x);
+    if (axis == CollisionAxis.X) {
 
-    // This thing isn't moving.
-    if (dir == 0) {
-        return result;
-    }
+        result.newPosition = entityPosition.x;
 
-    // Entity position is on the bottom center of the collisionbox.
-    immutable double entityHalfWidth = entitySize.x * 0.5;
-    immutable Rect entityRectangle = Rect(entityPosition.x - entityHalfWidth, entityPosition.y,
-        entitySize.x, entitySize.y);
+        int dir = cast(int) sgn(entityVelocity.x);
 
-    immutable Rect blockRectangle = Rect(blockPosition.x, blockPosition.y, blockSize.x, blockSize.y);
-
-    if (checkCollisionRecs(entityRectangle, blockRectangle)) {
-        // This doesn't kick out in a specific direction on dir 0 because the Y axis check will kick them up as a safety.
-        result.collides = true;
-        if (dir > 0) {
-            // Kick left.
-            result.newPosition = blockPosition.x - entityHalfWidth - magicAdjustment;
-        } else if (dir < 0) {
-            // Kick right.
-            result.newPosition = blockPosition.x + blockSize.x + entityHalfWidth + magicAdjustment;
+        // This thing isn't moving.
+        if (dir == 0) {
+            return result;
         }
+
+        // Entity position is on the bottom center of the collisionbox.
+        immutable double entityHalfWidth = entitySize.x * 0.5;
+        immutable Rect entityRectangle = Rect(entityPosition.x - entityHalfWidth, entityPosition.y,
+            entitySize.x, entitySize.y);
+
+        immutable Rect blockRectangle = Rect(blockPosition.x, blockPosition.y, blockSize.x, blockSize
+                .y);
+
+        if (checkCollisionRecs(entityRectangle, blockRectangle)) {
+            // This doesn't kick out in a specific direction on dir 0 because the Y axis check will kick them up as a safety.
+            result.collides = true;
+            if (dir > 0) {
+                // Kick left.
+                result.newPosition = blockPosition.x - entityHalfWidth - magicAdjustment;
+            } else if (dir < 0) {
+                // Kick right.
+                result.newPosition = blockPosition.x + blockSize.x + entityHalfWidth + magicAdjustment;
+            }
+        }
+    } else {
+
     }
 
     return result;
+
 }
 
 CollisionResult collideYToBlock(Vec2d entityPosition, Vec2d entitySize, Vec2d entityVelocity,
