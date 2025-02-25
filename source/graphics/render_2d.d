42f12ba79ecabd47933e0a67922265462b020a41
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-25 06:08:06 -0500

    Create direct functions

--- source/graphics/render_2d.d
+++ source/graphics/render_2d.d
@@ -8,4 +8,40 @@ private:
 
 public:
 
+    void drawLine(const double startX, const double startY, const double endX, const double endY,
+        const ref Color color) {
+
+        rlBegin(RL_LINES);
+        {
+            rlColor4ub(color.r, color.g, color.b, color.a);
+            rlVertex2f(startX, startY);
+            rlVertex2f(endX, endY);
+        }
+        rlEnd();
+    }
+
+    void drawRectangle(const double x, const double y, const double width, const double height, const ref Color color) {
+
+        const double topRightX = x + width;
+
+        const double bottomLeftY = y + height;
+
+        const double bottomRightX = x + width;
+        const double bottomRightY = y + height;
+
+        rlBegin(RL_TRIANGLES);
+        {
+            rlColor4ub(color.r, color.g, color.b, color.a);
+
+            rlVertex2f(x, y);
+            rlVertex2f(x, bottomLeftY);
+            rlVertex2f(topRightX, y);
+
+            rlVertex2f(topRightX, y);
+            rlVertex2f(x, bottomLeftY);
+            rlVertex2f(bottomRightX, bottomRightY);
+        }
+        rlEnd();
+    }
+
 }
