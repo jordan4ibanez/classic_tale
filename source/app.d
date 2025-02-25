3dead8eabcbaea53415b23f1c06b02a5100efb83
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-25 07:20:44 -0500

    Do everything in one shot

--- source/app.d
+++ source/app.d
@@ -177,20 +177,25 @@ void main() {
 
 		{ // Draw the crosshair.
 
+			import graphics.render_2d;
+
+			import std.datetime.stopwatch;
+
+			auto sw = StopWatch(AutoStart.yes);
+
 			Vec2d windowCenter = vec2dMultiply(Window.getSize(), Vec2d(0.5, 0.5));
 
-			double guiScale = GUI.getGUIScale();
+			const double guiScale = GUI.getGUIScale();
 
 			// Size horizontal.
 			//? 40, 4
 			const double shx = 40.0 * guiScale;
 			// Size vertical.
-			const double shy = 4.0 * guiScale;
+			const double shy = 6.0 * guiScale;
 			// Half width.
 			const double hx = shx * 0.5;
 			// Half height.
 			const double hy = shy * 0.5;
-			Vector2 start, end;
 
 			const double a = windowCenter.x - hx;
 			const double b = windowCenter.y - hy;
@@ -200,125 +205,77 @@ void main() {
 			const double f = windowCenter.x + hx;
 			const double g = windowCenter.y + hy;
 			const double h = windowCenter.y + hx;
+			const double i = hx - hy;
+
+			static const Color WHITE = Colors.WHITE;
+
+			// Left
+			(rectangles + 0).x = a;
+			(rectangles + 0).y = b;
+			(rectangles + 0).width = i;
+			(rectangles + 0).height = shy;
+
+			// Right
+			(rectangles + 1).x = e;
+			(rectangles + 1).y = b;
+			(rectangles + 1).width = i;
+			(rectangles + 1).height = shy;
+
+			// Vertical.
+			(rectangles + 2).x = c;
+			(rectangles + 2).y = d;
+			(rectangles + 2).width = shy;
+			(rectangles + 2).height = shx;
 
 			//? First pass uses subtractive blend mode.
 			//? This causes issues when looking at gray things like stone.
 			BeginBlendMode(BlendMode.BLEND_SUBTRACT_COLORS);
-			// Horizontal.
-			start.x = a;
-			start.y = b;
-			end.x = shx;
-			end.y = shy;
-			DrawRectangleV(start, end, Colors.WHITE);
-
-			// Vertical.
-			start.x = c;
-			start.y = d;
-			end.x = shy;
-			end.y = shx;
-			DrawRectangleV(start, end, Colors.WHITE);
-
-			// Center.
-			start.x = c;
-			start.y = b;
-			end.x = shy;
-			end.y = shy;
-			DrawRectangleV(start, end, Colors.WHITE);
+			Render2d.drawRectangles(rectangles, 3, WHITE);
 			EndBlendMode();
 
 			//? Second pass layers on lightness to make it stand out more.
-			static immutable ULTRA_DARK_GRAY = Color(55, 55, 55, 255);
+			static const ULTRA_DARK_GRAY = Color(55, 55, 55, 255);
+
 			BeginBlendMode(BlendMode.BLEND_ADD_COLORS);
-
-			// Left
-			start.x = a;
-			start.y = b;
-			end.x = hx - hy;
-			end.y = shy;
-			DrawRectangleV(start, end, ULTRA_DARK_GRAY);
-
-			// Right
-			start.x = e;
-			start.y = b;
-			end.x = hx - hy;
-			end.y = shy;
-			DrawRectangleV(start, end, ULTRA_DARK_GRAY);
-			// Vertical.
-			start.x = c;
-			start.y = d;
-			end.x = shy;
-			end.y = shx;
-			DrawRectangleV(start, end, ULTRA_DARK_GRAY);
-
+			Render2d.drawRectangles(rectangles, 3, ULTRA_DARK_GRAY);
 			EndBlendMode();
 
 			//? Third pass draws an outline on the cursor.
 
 			// This starts at the top left of the vertical bar then wraps around clockwise.
 
-			static immutable debugColor = Colors.BLACK;
+			static const Color debugColor = Colors.BLACK;
 
-			start.x = a;
-			start.y = b;
-			end.x = c;
-			end.y = b;
-			DrawLineV(start, end, debugColor);
-			start.x = c;
-			start.y = b;
-			end.x = c;
-			end.y = d;
-			DrawLineV(start, end, debugColor);
-			start.x = c;
-			start.y = d;
-			end.x = e;
-			end.y = d;
-			DrawLineV(start, end, debugColor);
-			start.x = e;
-			start.y = d;
-			end.x = e;
-			end.y = b;
-			DrawLineV(start, end, debugColor);
-			start.x = e;
-			start.y = b;
-			end.x = f;
-			end.y = b;
-			DrawLineV(start, end, debugColor);
-			start.x = f;
-			start.y = b;
-			end.x = f;
-			end.y = g;
-			DrawLineV(start, end, debugColor);
-			start.x = f;
-			start.y = g;
-			end.x = e;
-			end.y = g;
-			DrawLineV(start, end, debugColor);
-			start.x = e;
-			start.y = g;
-			end.x = e;
-			end.y = h;
-			DrawLineV(start, end, debugColor);
-			start.x = e;
-			start.y = h;
-			end.x = c;
-			end.y = h;
-			DrawLineV(start, end, debugColor);
-			start.x = c;
-			start.y = h;
-			end.x = c;
-			end.y = g;
-			DrawLineV(start, end, debugColor);
-			start.x = c;
-			start.y = g;
-			end.x = a;
-			end.y = g;
-			DrawLineV(start, end, debugColor);
-			start.x = a;
-			start.y = g;
-			end.x = a;
-			end.y = b;
-			DrawLineV(start, end, debugColor);
+			(linePoints + 0).x = a;
+			(linePoints + 0).y = b;
+			(linePoints + 1).x = c;
+			(linePoints + 1).y = b;
+			(linePoints + 2).x = c;
+			(linePoints + 2).y = d;
+			(linePoints + 3).x = e;
+			(linePoints + 3).y = d;
+			(linePoints + 4).x = e;
+			(linePoints + 4).y = b;
+			(linePoints + 5).x = f;
+			(linePoints + 5).y = b;
+			(linePoints + 6).x = f;
+			(linePoints + 6).y = g;
+			(linePoints + 7).x = e;
+			(linePoints + 7).y = g;
+			(linePoints + 8).x = e;
+			(linePoints + 8).y = h;
+			(linePoints + 9).x = c;
+			(linePoints + 9).y = h;
+			(linePoints + 10).x = c;
+			(linePoints + 10).y = g;
+			(linePoints + 11).x = a;
+			(linePoints + 11).y = g;
+			(linePoints + 12).x = a;
+			(linePoints + 12).y = b;
+
 			Render2d.drawLinesConnected(linePoints, 13, debugColor);
+
+			writeln("took: ", cast(double) sw.peek().total!"hnsecs", " hns");
 		}
 
 		EndDrawing();
